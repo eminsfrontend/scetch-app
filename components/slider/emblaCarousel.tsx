d543e142e7cbe546d40bdd173ddf888946e38cf3
eminsfrontend <eminsfrontend@gmail.com>
2022-11-29 21:30:03 +0300

    Embla change

--- components/slider/emblaCarousel.tsx
+++ components/slider/emblaCarousel.tsx
@@ -1,22 +1,22 @@
-import {}
-import useEmblaCarousel from "embla-carousel-react/components"
+// import {}
+// // import useEmblaCarousel from "embla-carousel-react/components"
 
-export default function Slider() {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false })
+// export default function Slider() {
+//   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false })
 
-  useEffect(() => {
-    if (emblaApi) {
-      // Embla API is ready
-    }
-  }, [emblaApi])
+//   useEffect(() => {
+//     if (emblaApi) {
+//       // Embla API is ready
+//     }
+//   }, [emblaApi])
 
-  return (
-    <div className="overflow-hidden" ref={emblaRef}>
-      <div className="flex">
-        <div className="flex-[0 0 100%]">Slide 1</div>
-        <div className="flex-[0 0 100%]">Slide 2</div>
-        <div className="flex-[0 0 100%]">Slide 3</div>
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div className="overflow-hidden" ref={emblaRef}>
+//       <div className="flex">
+//         <div className="flex-[0 0 100%]">Slide 1</div>
+//         <div className="flex-[0 0 100%]">Slide 2</div>
+//         <div className="flex-[0 0 100%]">Slide 3</div>
+//       </div>
+//     </div>
+//   )
+// }
